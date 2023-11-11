cd3dc56d28bfb08933002f001f3760fbf7401a2b
jameshsu1125 <jameshsu1125@gmail.com>
2023-11-11 14:31:48 +0800

    Update table.tsx

--- src/pages/home/table.tsx
+++ src/pages/home/table.tsx
@@ -1,5 +1,5 @@
 import { Prettify } from '@/settings/type';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { SETTING } from '../../../setting';
 import './index.less';
 
@@ -9,6 +9,24 @@ const Table = memo(({ data }: Prettify<{ data: T }>) => {
   const [currentData] = data;
   const keys = Object.keys(currentData);
   const { collection } = currentData;
+
+  const { schema } = currentData;
+  const table = useMemo(() => {
+    return Object.entries(schema).map((each) => {
+      const [tableName, tableValue] = each;
+      const { type, required, default: def } = tableValue;
+      let requiredDefault = '';
+
+      if (required !== undefined) {
+        requiredDefault = 'required : ' + JSON.stringify(required).split('"').join('');
+      }
+
+      if (def !== undefined) {
+        requiredDefault = 'default : ' + JSON.stringify(def).split('"').join('');
+      }
+      return [tableName, type, requiredDefault];
+    });
+  }, [schema]);
 
   return (
     <div className='mockup-browser border border-base-300'>
@@ -31,7 +49,35 @@ const Table = memo(({ data }: Prettify<{ data: T }>) => {
               <tr>
                 <td>{currentData.collection}</td>
                 <td>{currentData.description}</td>
-                <td>{JSON.stringify(currentData.schema)}</td>
+                <td>
+                  <div className='overflow-x-auto'>
+                    <table className='table table-xs'>
+                      <thead>
+                        <tr>
+                          <th>table name</th>
+                          <th>type</th>
+                          <th>setting</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {table.map((each) => (
+                          <tr key={JSON.stringify(each)}>
+                            {each.map((v, index) => (
+                              <td key={`${v}${index}`}>{v}</td>
+                            ))}
+                          </tr>
+                        ))}
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th>table name</th>
+                          <th>type</th>
+                          <th>setting</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </td>
               </tr>
             </tbody>
           </table>
