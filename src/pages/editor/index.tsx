117189c838a7a4936a2469c5efe32eaaf4d32b66
James Hsu <jameshsu1125@gmail.com>
2025-09-23 14:27:28 +0800

    feat: 更新多個依賴版本，刪除不再使用的編輯器組件及樣式

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,14 +1,4 @@
-import Editor from '@/components/richEditor';
 import { memo } from 'react';
 
-const EditorPage = memo(() => (
-  <div className='w-full'>
-    <Editor
-      defaultHTML=''
-      onSubmit={(html) => {
-        console.log(html);
-      }}
-    />
-  </div>
-));
+const EditorPage = memo(() => <div className='w-full'></div>);
 export default EditorPage;
