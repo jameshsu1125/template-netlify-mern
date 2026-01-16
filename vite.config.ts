fffdaa1e2f07941e02d2af300aba1e758fa26743
jameshsu1125 <jameshsu1125@gmail.com>
2026-01-16 14:20:01 +0800

    feat: 更新多個依賴版本以提升穩定性，新增 Deno 鎖定檔案

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import react from '@vitejs/plugin-react';
-import { certificateFor } from 'devcert';
 import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, './src/pages');
-  const { key, cert } = await certificateFor('localhost');
 
   return {
     base: './',
@@ -34,6 +33,7 @@ export default defineConfig(async ({ mode }) => {
     },
     plugins: [
       react(),
+      mkcert(),
       createHtmlPlugin({
         minify: true,
         inject: {
@@ -55,7 +55,6 @@ export default defineConfig(async ({ mode }) => {
     server: {
       open: true,
       port: 5173,
-      https: { key, cert },
       proxy: {
         '/api': 'http://localhost:8888',
       },
