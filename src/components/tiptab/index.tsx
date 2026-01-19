d3f8dc7ed38c796a678d21dda191516adba9a37f
jameshsu1125 <jameshsu1125@gmail.com>
2026-01-19 10:12:02 +0800

    feat: 更新 Tiptap 編輯器組件以支持內容儲存功能，並整合相關邏輯

--- src/components/tiptab/index.tsx
+++ src/components/tiptab/index.tsx
@@ -4,27 +4,36 @@ import StarterKit from '@tiptap/starter-kit';
 import './index.less';
 import MenuBar from './menu';
 import { FaRegSave } from 'react-icons/fa';
+import { useEffect } from 'react';
 
 const extensions = [TextStyle, StarterKit];
 
-export default ({ onSave }: { onSave: (html: string) => void }) => {
+export default ({ onSave, html }: { onSave: (html: string) => void; html: string }) => {
   const editor = useEditor({
     extensions,
-    content: ``,
+    content: html,
   });
 
+  useEffect(() => {
+    if (editor && html !== editor.getHTML()) {
+      editor.commands.setContent(html);
+    }
+  }, [html, editor]);
+
   return (
-    <div className='flex w-full flex-col gap-2'>
-      <div className='flex flex-col'>
-        <MenuBar editor={editor} />
-      </div>
-      <div className='bg-base-200 p-5'>
-        <EditorContent editor={editor} className='bg-base-100 rounded-lg [&_div]:min-h-44' />
-      </div>
-      <div className='flex w-full justify-end'>
-        <button className='btn' onClick={() => onSave(editor?.getHTML() || '')}>
-          <FaRegSave /> 儲存
-        </button>
+    <div className='bg-base-300 p-5'>
+      <div className='flex w-full flex-col gap-2'>
+        <div className='flex flex-col'>
+          <MenuBar editor={editor} />
+        </div>
+        <div className='bg-base-200 p-5'>
+          <EditorContent editor={editor} className='bg-base-100 rounded-lg [&_div]:min-h-44' />
+        </div>
+        <div className='flex w-full justify-end'>
+          <button className='btn' onClick={() => onSave(editor?.getHTML() || '')}>
+            <FaRegSave /> 儲存
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,12 +1,20 @@
 import Tiptap from '@/components/tiptab';
 import useInsert from '@/hooks/useInsert';
+import useSelect from '@/hooks/useSelect';
 import { Context } from '@/settings/constant';
 import { ActionType, AlertType } from '@/settings/type';
-import { memo, useContext, useEffect } from 'react';
+import { memo, useCallback, useContext, useEffect } from 'react';
+import { TType } from '../../../setting';
+import useUpdate from '@/hooks/useUpdate';
 
 const EditorPage = memo(() => {
   const [, setContext] = useContext(Context);
   const [response, insert] = useInsert();
+  const [responseUpdate, update] = useUpdate();
+  const [data, setData] = useSelect();
+
+  const currentData = data?.data as Extract<TType, { html: string }>[];
+
   useEffect(() => {
     if (response) {
       if (response.res) {
@@ -16,14 +24,37 @@ const EditorPage = memo(() => {
         });
       }
     }
-  }, [response]);
+    if (responseUpdate) {
+      if (responseUpdate.res) {
+        setContext({
+          type: ActionType.Alert,
+          state: { enabled: true, type: AlertType.Success, body: responseUpdate.msg },
+        });
+      }
+    }
+  }, [response, responseUpdate]);
+
+  useEffect(() => {
+    setData({ collection: 'editor' });
+  }, []);
+
+  const onSave = useCallback(
+    (html: string) => {
+      if (currentData && currentData.length > 0) {
+        update({
+          collection: 'editor',
+          data: { _id: (currentData[0] as any)._id, data: { html } },
+        });
+      } else {
+        insert({ collection: 'editor', data: { html } });
+      }
+    },
+    [currentData, insert, update],
+  );
+
   return (
-    <div className='w-full'>
-      <Tiptap
-        onSave={(html) => {
-          insert({ collection: 'editor', data: { html } });
-        }}
-      />
+    <div className=''>
+      <Tiptap html={currentData?.[0]?.html || ''} onSave={onSave} />
     </div>
   );
 });
